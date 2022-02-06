1818a1fe187f299dd41f5663059aae5df3ce43e1
Caio Gomes <caiogame100@gmail.com>
2022-02-06 20:25:12 -0300

    Initial Commit

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="Header">
+      <h1>Filmaria</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+}
+</script>
+
+<style scoped>
+#Header {
+    background-color: #fff;
+    box-shadow: 0px 1px 5px rgba(0,0,0,.1);
+    padding: 25px 0px;
+    text-align: center;
+}
+
+</style>
